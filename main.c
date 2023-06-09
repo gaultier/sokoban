5889212f5cdf7c73e0da918708812f6b7fd2feb1
Philippe Gaultier <philippe.gaultier@gi-de.com>
2023-06-09 11:39:07 +0200

    rename

--- main.c
+++ main.c
@@ -30,10 +30,12 @@ typedef enum : uint8_t {
   ENTITY_MAX, // Not an entity.
 } Entity;
 
-static bool entity_is_at_least(Entity a, Entity b) { return (a & b) == b; }
-static bool entity_is_exactly(Entity a, Entity b) { return a == b; }
-static void entity_remove_other(Entity *a, Entity b) { *a &= ~b; }
-static void entity_add_other(Entity *a, Entity b) { *a |= b; }
+static bool bitset_contains(Entity bitset, Entity b) {
+  return (bitset & b) == b;
+}
+static bool bitset_is_exactly(Entity bitset, Entity b) { return bitset == b; }
+static void bitset_remove(Entity *bitset, Entity b) { *bitset &= ~b; }
+static void bitset_add(Entity *bitset, Entity b) { *bitset |= b; }
 
 #define MAP_WIDTH 12
 #define MAP_HEIGHT 12
@@ -133,35 +135,35 @@ static void go(Direction dir, uint8_t *character_cell_i, Entity *map) {
   uint8_t next_cell_i = get_next_cell_i(dir, *character_cell_i);
   Entity *const next_cell = &map[next_cell_i];
   // MW => No pathing.
-  if (entity_is_exactly(*next_cell, ENTITY_WALL))
+  if (bitset_is_exactly(*next_cell, ENTITY_WALL))
     return;
 
   // MN, MO => Free pathing.
-  if (entity_is_exactly(*next_cell, ENTITY_NONE) ||
-      entity_is_exactly(*next_cell, ENTITY_OBJECTIVE)) {
-    entity_remove_other(&map[*character_cell_i], ENTITY_CHARACTER);
-    entity_add_other(next_cell, ENTITY_CHARACTER);
+  if (bitset_is_exactly(*next_cell, ENTITY_NONE) ||
+      bitset_is_exactly(*next_cell, ENTITY_OBJECTIVE)) {
+    bitset_remove(&map[*character_cell_i], ENTITY_CHARACTER);
+    bitset_add(next_cell, ENTITY_CHARACTER);
 
     *character_cell_i = next_cell_i;
     return;
   }
 
-  // MC*, MCo* from this point on.
+  // MC* from this point on.
 
   Entity *const next_next_cell = &map[get_next_cell_i(dir, next_cell_i)];
 
-  // MCW, MCC, MCCo, MCoW, MCoC, MCoCo => No pathing.
-  if (entity_is_exactly(*next_next_cell, ENTITY_WALL) ||
-      entity_is_at_least(*next_next_cell, ENTITY_CRATE))
+  // MCW, MCC => No pathing.
+  if (bitset_is_exactly(*next_next_cell, ENTITY_WALL) ||
+      bitset_contains(*next_next_cell, ENTITY_CRATE))
     return;
 
-  // MCN, MCoN, MCO, MCoO  => Advance the crate.
-  if (entity_is_exactly(*next_next_cell, ENTITY_NONE) ||
-      entity_is_at_least(*next_next_cell, ENTITY_OBJECTIVE)) {
-    entity_remove_other(&map[*character_cell_i], ENTITY_CHARACTER);
-    entity_remove_other(next_cell, ENTITY_CRATE);
-    entity_add_other(next_cell, ENTITY_CHARACTER);
-    entity_add_other(next_next_cell, ENTITY_CRATE);
+  // MCN, MCO => Advance the crate.
+  if (bitset_is_exactly(*next_next_cell, ENTITY_NONE) ||
+      bitset_contains(*next_next_cell, ENTITY_OBJECTIVE)) {
+    bitset_remove(&map[*character_cell_i], ENTITY_CHARACTER);
+    bitset_remove(next_cell, ENTITY_CRATE);
+    bitset_add(next_cell, ENTITY_CHARACTER);
+    bitset_add(next_next_cell, ENTITY_CRATE);
 
     *character_cell_i = next_cell_i;
   }
@@ -241,11 +243,11 @@ int main() {
     for (uint8_t i = 0; i < MAP_WIDTH * MAP_HEIGHT; i++) {
       const Entity cell = game_map[i];
 
-      if (entity_is_exactly(cell, ENTITY_NONE)) // Nothing to render.
+      if (bitset_is_exactly(cell, ENTITY_NONE)) // Nothing to render.
         continue;
 
       // Since we are iterating on each cell, count the number of crates ok.
-      crates_ok_count += entity_is_exactly(cell, ENTITY_CRATE_OK);
+      crates_ok_count += bitset_is_exactly(cell, ENTITY_CRATE_OK);
 
       const SDL_Rect rect = {.w = CELL_SIZE,
                              .h = CELL_SIZE,
@@ -257,15 +259,15 @@ int main() {
       // There is a bit of precedence here: in the case of multiple entities
       // occupying the same cell, we want to draw: character > crate_ok > crate
       // > objective.
-      if (entity_is_at_least(cell, ENTITY_CHARACTER)) {
+      if (bitset_contains(cell, ENTITY_CHARACTER)) {
         SDL_RenderCopy(renderer, current, NULL, &rect);
-      } else if (entity_is_exactly(cell, ENTITY_WALL)) {
+      } else if (bitset_is_exactly(cell, ENTITY_WALL)) {
         SDL_RenderCopy(renderer, textures[ENTITY_WALL], NULL, &rect);
-      } else if (entity_is_exactly(cell, ENTITY_CRATE_OK)) {
+      } else if (bitset_is_exactly(cell, ENTITY_CRATE_OK)) {
         SDL_RenderCopy(renderer, textures[ENTITY_CRATE_OK], NULL, &rect);
-      } else if (entity_is_exactly(cell, ENTITY_CRATE)) {
+      } else if (bitset_is_exactly(cell, ENTITY_CRATE)) {
         SDL_RenderCopy(renderer, textures[ENTITY_CRATE], NULL, &rect);
-      } else if (entity_is_exactly(cell, ENTITY_OBJECTIVE)) {
+      } else if (bitset_is_exactly(cell, ENTITY_OBJECTIVE)) {
         SDL_RenderCopy(renderer, textures[ENTITY_OBJECTIVE], NULL, &rect);
       }
     }
